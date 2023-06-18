744f3cc01e7361af5703304232d7b3ed85d5c45a
Christapher Antony <christapher012@gmail.com>
2023-06-18 21:48:38 +0530

    [test] - integrated ci

--- src/config/cors.options.js
+++ src/config/cors.options.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://www.testserver1.cf','https://messaging-app-theta.vercel.app'],
+    origin: ['http://localhost:3000', 'https://testserver2.tk/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
 };
